32346b2af5d4b70fe475bae29490f9ba7f7b5c4e
Rosanna Pistone <rosanna.pistone@hiq.se>
2024-10-08 10:26:58 +0200

    downgraded react for codepen

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom/client';
+// import './index.css';
+// import App from './App';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import ReactDOM from 'react-dom';
 import App from './App';
+import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+ReactDOM.render(<App />, document.getElementById('root'));
 
+// const root = ReactDOM.render(document.getElementById('root'));
+// root.render(<App />);
