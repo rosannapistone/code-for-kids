831bb42f33ec167f1a38f92541af1e10857c42a8
Rosanna Pistone <rosanna.pistone@hiq.se>
2024-10-08 11:01:34 +0200

    fixed eslint error

--- src/Components/symbols/Symbols.jsx
+++ src/Components/symbols/Symbols.jsx
@@ -28,20 +28,20 @@ export const Symbols = ({showJS}) => {
         <Logo.CSS className="css-icon" />
         <h4>CSS</h4>
         <p>CSS är ett språk som gör hemsidor fina. Det bestämmer färg, storlek och var saker ska vara.</p>
-        <a href="https://www.w3schools.com/css/default.asp" target="_blank">Läs mer om CSS</a>
+        <a href="https://www.w3schools.com/css/default.asp" target="_blank" rel="noreferrer">Läs mer om CSS</a>
         </div>
         <div className="html">
         <Logo.HTML className="html-icon"/>
         <h4>HTML</h4>
         <p>HTML är som ett skelett för hemsidor. Det bestämmer vad som ska finnas, som text och bilder.</p>
-        <a href="https://www.w3schools.com/html/default.asp" target="_blank">Läs mer om HTML</a>
+        <a href="https://www.w3schools.com/html/default.asp" target="_blank" rel="noreferrer">Läs mer om HTML</a>
         </div>
         {showJS === true ? (
            <div className="javascript">
           <Logo.JS className="js-icon"/>
           <h4>JAVASCRIPT</h4>
           <p>JavaScript är hjärnan för hemsidor. Det gör så saker kan hända, som att knappar fungerar och spel kan spelas.</p>
-          <a href="https://www.w3schools.com/js/default.asp" target="_blank">Läs mer om JavaScript</a>
+          <a href="https://www.w3schools.com/js/default.asp" target="_blank" rel="noreferrer">Läs mer om JavaScript</a>
           </div> 
         )
       : null}
