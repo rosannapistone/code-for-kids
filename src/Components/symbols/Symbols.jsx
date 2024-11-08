cc92ca157070d70eaab2e8f368d983849a5e1245
Rosanna Pistone <rosanna.pistone@hiq.se>
2024-11-08 16:06:38 +0100

    removed eslint, favicon, name

--- src/Components/symbols/Symbols.jsx
+++ src/Components/symbols/Symbols.jsx
@@ -1,56 +1,66 @@
-import { useState } from "react"
-import { Icon } from "../../Assets/icons/Icons"
-import { Illustration } from "../../Assets/illustrations/illustrations"
-import { Logo } from "../../Assets/logos/logos"
-import "./symbols.scss"
+import { useState } from 'react';
+import { Icon } from '../../Assets/icons/Icons';
+import { Illustration } from '../../Assets/illustrations/illustrations';
+import { Logo } from '../../Assets/logos/logos';
+import './symbols.scss';
 
-export const Symbols = ({showJS}) => {
-
+export const Symbols = () => {
   const [show, setShow] = useState(true);
 
-
-  return (    
+  return (
     <div className="symbols-container">
-    
       {!show ? (
-        <Icon.Info onClick={() => setShow(true)}/>
+        <div className="icon-target-area" onClick={() => setShow(true)}>
+          <Icon.Info />
+        </div>
       ) : (
-        <Icon.Close onClick={() => setShow(false)} />
+        <div className="icon-target-area" onClick={() => setShow(false)}>
+          <Icon.Close />
+        </div>
       )}
-      {show ? (<div className="symbols">
-        <div className="inner">
-        <div className="light-bulb">
-        <Illustration.LightBulbBlue />
-        <h4>GLÖDLAMPAN</h4>
-        <p>Glödlampan ger tips och hjälp om ni fastnar i kodutmaningarna. Den finns tillgänglig i alla utmaningar.</p>
-        </div>
-        <div className="css">
-        <Logo.CSS className="css-icon" />
-        <h4>CSS</h4>
-        <p>CSS är ett språk som gör hemsidor fina. Det bestämmer färg, storlek och var saker ska vara.</p>
-        <a href="https://www.w3schools.com/css/default.asp" target="_blank" rel="noreferrer">Läs mer om CSS</a>
-        </div>
-        <div className="html">
-        <Logo.HTML className="html-icon"/>
-        <h4>HTML</h4>
-        <p>HTML är som ett skelett för hemsidor. Det bestämmer vad som ska finnas, som text och bilder.</p>
-        <a href="https://www.w3schools.com/html/default.asp" target="_blank" rel="noreferrer">Läs mer om HTML</a>
-        </div>
-        {showJS === true ? (
-           <div className="javascript">
-          <Logo.JS className="js-icon"/>
-          <h4>JAVASCRIPT</h4>
-          <p>JavaScript är hjärnan för hemsidor. Det gör så saker kan hända, som att knappar fungerar och spel kan spelas.</p>
-          <a href="https://www.w3schools.com/js/default.asp" target="_blank" rel="noreferrer">Läs mer om JavaScript</a>
-          </div> 
-        )
-      : null}
-       
+      {show ? (
+        <div className="symbols">
+          <div className="inner">
+            <div className="light-bulb">
+              <Illustration.LightBulbBlue />
+              <h4>GLÖDLAMPAN</h4>
+              <p>
+                Glödlampan ger tips och hjälp om du fastnar i kodutmaningarna.
+              </p>
+            </div>
+            <div className="css">
+              <Logo.CSS className="css-icon" />
+              <h4>CSS</h4>
+              <p>
+                CSS är ett språk som gör hemsidor fina. Det bestämmer färg,
+                storlek och var saker ska vara.
+              </p>
+              <a
+                href="https://www.w3schools.com/css/default.asp"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Läs mer om CSS
+              </a>
+            </div>
+            <div className="html">
+              <Logo.HTML className="html-icon" />
+              <h4>HTML</h4>
+              <p>
+                HTML är som ett skelett för hemsidor. Det bestämmer vad som ska
+                finnas, som text och bilder.
+              </p>
+              <a
+                href="https://www.w3schools.com/html/default.asp"
+                target="_blank"
+                rel="noreferrer"
+              >
+                Läs mer om HTML
+              </a>
+            </div>
           </div>
-          </div>) : (null)}
-          
-       
-          </div>
-  
-)
-}
+        </div>
+      ) : null}
+    </div>
+  );
+};
